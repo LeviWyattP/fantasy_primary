f3c13414130109193be376c109e3fa961d9ea809
AlexiaDunlap <DunlapAlexia@yahoo.com>
2018-12-31 19:52:21 -0500

    Cleaned up code according to PEP style Added League class into flask start page.

--- server.py
+++ server.py
@@ -1,16 +1,37 @@
 __author__ = 'Levi'
 
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, g, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_oidc import OpenIDConnect
 import data_stores as data
 import os
 import time
 import test_class as tc
+import auth_secrets as aus
 import Web_classes as wc
 import primary_results as testdata
+from okta import UsersClient
+
 
 app = Flask(__name__)
 Bootstrap(app)
+oidc, okta_client = aus.run_config(app)
+
+players = ['Levi', 'Lex', 'Eric']
+gplay = tc.League()
+gplay.build_players(players, 20)
+gplay.build_candidates(data.candidates, data.stock_price_modifiers)
+gplay.build_states()
+gplay.update_state_data("Iowa", testdata.iowa)
+
+
+
+@app.before_request
+def before_request():
+    if oidc.user_loggedin:
+        g.user = okta_client.get_user(str(oidc.user_getfield("sub")))
+    else:
+        g.user = None
 
 
 @app.route('/favicon.ico')
@@ -20,18 +41,16 @@ def favicon():
 
 
 @app.route("/", methods=['GET', 'POST'])
+@oidc.require_login
 def hello():
     user_ip = request.remote_addr
     with open('Server_Log_File.log', 'w') as logfile:
             logfile.write("Got connection from user at IP address: " + user_ip + " at time: " +
                           time.strftime("%c") + "\n")
-    players = ['Levi', 'Lex', 'Eric']
-    gplay = tc.League()
-    gplay.build_players(players, 20)
-    gplay.build_candidates(data.candidates, data.stock_price_modifiers)
-    gplay.build_states()
-    gplay.update_state_data("Iowa", testdata.iowa)
 
+
+    print(g.user)
+    print(g.user.profile.firstName)
     game_people = []
     for player in gplay.player_names:
         game_people.append(gplay.players[player].get_web_class())
@@ -44,10 +63,12 @@ def hello():
 
 
     # TODO maybe we should get candidate info from json? Is there added value from getting it from the class?
-    return render_template('index.html', upcoming_primaries = upcoming_primaries, people=game_people,candidates=game_candidates)
+    return render_template('index.html', upcoming_primaries = upcoming_primaries, people=game_people,
+                           candidates=game_candidates)
 
 
 @app.route('/home', methods=['GET', 'POST'])
+@oidc.require_login
 def index():
     a = request.remote_addr
     with open('Server_Log_File.log', 'a') as logfile:
@@ -56,7 +77,24 @@ def index():
     return render_template('home.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    user_ip = request.remote_addr
+    with open('Server_Log_File.log', 'w') as logfile:
+        logfile.write("Got connection from user at IP address: " + user_ip + " at time: " +
+                      time.strftime("%c") + "\n")
+    return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    a=1
+    oidc.logout()
+    return redirect(url_for(".hello"))
+
+
 @app.route('/userprofile', methods=['GET', 'POST'])
+@oidc.require_login
 def userprofile():
     a = request.remote_addr
     with open('Server_Log_File.log', 'a') as logfile:
@@ -76,7 +114,7 @@ def userprofile():
 
     user = gplay.players['Lex'].get_web_class()
 
-    return render_template('profile.html', user = user)
+    return render_template('profile.html', user=user)
 
 if __name__ == '__main__':
     app.debug = True
